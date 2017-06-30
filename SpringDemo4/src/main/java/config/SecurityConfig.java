c5805bd183fc2df5de7f4f4cd1acd29d9573594d
wangyu <123456>
2017-07-01 01:12:40 +0800

    aa

--- SpringDemo4/src/main/java/config/SecurityConfig.java
+++ SpringDemo4/src/main/java/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -31,13 +32,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .formLogin()
+//                .and()
+//                .httpBasic();
         http.authorizeRequests()
-                .anyRequest()
+                .antMatchers("/Demo1/*")    // 匹配/Demo1/, 不匹配/Demo1
+                .hasAuthority("admin")
+                .antMatchers("/Demo1/**")   // 匹配/Demo1/, 也匹配/Demo1
                 .authenticated()
+                .antMatchers(HttpMethod.POST,"/shop/**")
+                .authenticated()
+                .anyRequest()
+                .permitAll()
                 .and()
-                .formLogin()
-                .and()
-                .httpBasic();
+                .formLogin();
     }
 
     @Override
@@ -47,16 +59,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // 内存用户校验
 //        auth.inMemoryAuthentication().withUser("user").password("1111").roles("user");
 
+        // 关系型数据库用户校验
+        // 用户权限的表结构和默认表结构完全相同，使用默认sql语句，查询用户和权限信息
+        // 默认表结构，可参看spring_security.sql
 //        auth.jdbcAuthentication().dataSource(dataSource);
 
+        // 关系型数据库用户校验
+        // 自定义表结构，自定义sql语句
 //        auth.jdbcAuthentication()
 //                .dataSource(dataSource)
 //                .usersByUsernameQuery("select username, password, true from users where username = ?")
 //                .authoritiesByUsernameQuery("select username, authority from authorities where username = ?")
 //                .passwordEncoder(NoOpPasswordEncoder.getInstance());
 
+        // 自定义查询，通过用户名查询用户角色信息
         auth.userDetailsService(new UserDetailsServiceImpl());
     }
 
--- SpringDemo4/src/main/java/security/UserDetailsServiceImpl.java
+++ SpringDemo4/src/main/java/security/UserDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package security;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,7 +8,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by admin on 2017/6/22.
@@ -17,7 +20,21 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return new User(s, "1111",
-                Arrays.asList(new SimpleGrantedAuthority("auth")));
+        if (s == null || "".equals(s)) {
+            throw new UsernameNotFoundException(s + "not found");
+        }
+
+        List<GrantedAuthority> userAuthList = new ArrayList<>();
+        userAuthList.add(new SimpleGrantedAuthority("user"));
+        userAuthList.add(new SimpleGrantedAuthority("normal"));
+
+        List<GrantedAuthority> adminAuthList = new ArrayList<>();
+        adminAuthList.add(new SimpleGrantedAuthority("admin"));
+
+        if (s.equals("admin")) {
+            return new User(s, "1111", adminAuthList);
+        } else {
+            return new User(s, "1111", userAuthList);
+        }
     }
 }
